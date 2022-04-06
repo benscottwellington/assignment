c866c49496d79e56e0ae55166a77a7868ef52062
18345 <18345@wc.school.nz>
2022-04-06 13:33:52 +1200

    Database created + login

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+from sqlite3 import Error
+
+DATABASE = "website.db"
+
 app = Flask(__name__)
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+
+    return None
 
 @app.route('/')
 def web_main_page():
-    return render_template('base.html')
+
+    con = create_connection(DATABASE)
+
+    query = "SELECT category FROM dictionary"
+
+    cur = con.cursor()
+    cur.execute(query)
+    category_list = cur.fetchall()
+    con.close()
+
+    return render_template('home.html', category = category_list)
 
 @app.route('/login')
 def web_login():
