d20291749d49a8a004399ed92016d794a9755c5c
18345 <18345@wc.school.nz>
2022-04-14 11:24:09 +1200

    More categories work

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
-
-DATABASE = "website.db"
+from flask_bcrypt import Bcrypt
+from datetime import datetime
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+DATABASE = "website.db"
+app.secret_key = "srugy8rthy8gyrsuaisgbi89ujdf"
 
 def create_connection(db_file):
     try:
@@ -15,11 +18,7 @@ def create_connection(db_file):
 
     return None
 
-@app.route('/', methods=['POST', 'GET'])
-def web_main_page():
-    if request.method == 'POST':
-        category = request.form.get('category').lower().strip()
-
+def get_category_list():
     con = create_connection(DATABASE)
 
     query = "SELECT category, id FROM categories ORDER BY category ASC "
@@ -28,19 +27,34 @@ def web_main_page():
     cur.execute(query)
     category_list = cur.fetchall()
     con.close
+    return category_list
 
-    return render_template('home.html', categories=category_list)
+@app.route('/', methods=['POST', 'GET'])
+def web_main_page():
+    if request.method == 'POST':
+        category = request.form.get('category').lower().strip()
+
+    return render_template('home.html', categories=get_category_list())
+
+@app.route('/category')
+def web_category_page():
+    con = create_connection(DATABASE)
+    query = "SELECT maori_word, english_word, decription, level, id FROM words"
+    cur = con.cursor()
+    cur.execute(query)
+
+    words_list = cur.fetchall()
+    cur.close()
+
+
+    return render_template('category.html', categories=get_category_list(), words=words_list)
+
+
 
 @app.route('/category/<catID>')
 def web_categories_page(catID):
 
     con = create_connection(DATABASE)
-
-    query = "SELECT category, id FROM categories ORDER BY category ASC "
-
-    cur = con.cursor()
-    cur.execute(query)
-    category_list = cur.fetchall()
 
     query = "SELECT maori_word, english_word, description, level, id " \
             "FROM words WHERE catID=? ORDER BY maori_word ASC"
@@ -50,15 +64,18 @@ def web_categories_page(catID):
     words_list = cur.fetchall
     con.close
 
-    return render_template('home.html', words=words_list, categories=category_list)
+    return render_template('category.html', words_list=words_list, categories=get_category_list())
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def web_login():
+    if is_logged_in():
+        return redirect('/')
+
     if request.method == 'POST':
         email = request.form.get('email').lower().strip()
         password = request.form.get('password')
 
-        query = "SELECT id, first_name, password FROM customer WHERE email =?"
+        query = "SELECT id, first_name, password FROM user WHERE email =?"
         con = create_connection(DATABASE)
         cur = con.cursor()
         cur.execute(query, (email, ))
@@ -66,7 +83,7 @@ def web_login():
         con.close()
 
         try:
-            customer_id = user_data[0][0]
+            user_id = user_data[0][0]
             first_name = user_data[0][1]
             db_password = user_data[0][2]
 
@@ -77,11 +94,12 @@ def web_login():
             return redirect(request.referrer + "?error=Email+or+password+incorrect")
 
         session['email'] = email
-        session['customer_id'] = customer_id
+        session['user_id'] = user_id
         session['first_name'] = first_name
         print(session)
         return redirect("/")
-    return render_template('login.html',logged_in=is_logged_in())
+
+    return render_template('login.html', categories=get_category_list())
 
 @app.route('/signup', methods=['POST', 'GET'])
 def web_signup():
@@ -106,7 +124,7 @@ def web_signup():
 
         con = create_connection(DATABASE)
 
-        query = "INSERT INTO customer (first_name, last_name, email, password) " \
+        query = "INSERT INTO user (first_name, last_name, email, password) " \
                 "VALUES (?, ?, ?, ?)"
 
         cur = con.cursor()
@@ -114,6 +132,8 @@ def web_signup():
             cur.execute(query, (fname, lname, email, hashed_password))
         except sqlite3.IntegrityError:
             return redirect('/signup?error=email+is+already+in+use')
+
+    return render_template('signup.html')
 
 def is_logged_in():
     if session.get("email") is None:
